2227a6a6856ba633bc029fc54f58ef3ba689c586
Joris Snellenburg <jsnel@users.noreply.github.com>
2023-12-03 15:05:11 +0100

    Update notebook plotting step 1

--- PSI_TA_Scy6803GTA/custom_plotting.py
+++ PSI_TA_Scy6803GTA/custom_plotting.py
@@ -5,41 +5,30 @@ from pyglotaran_extras.plotting.plot_concentrations import plot_concentrations
 from pyglotaran_extras.plotting.plot_residual import plot_residual
 from pyglotaran_extras.plotting.plot_spectra import plot_das, plot_sas
 from pyglotaran_extras.plotting.plot_svd import plot_lsv_residual, plot_rsv_residual
-from pyglotaran_extras.plotting.style import ColorCode
-
-# myFRLcolors = [ "tab:grey","tab:orange",  ColorCode.cyan, ColorCode.green,"m", "y", "k","r", "b", "tab:purple"]
-myFRLcolors = ["g", "r", "k", ColorCode.cyan, "b"]
-
-custom_cycler = cycler(color=myFRLcolors)
-# custom_cycler2 = cycler(color=["k","r", ColorCode.green,"b","m"])
-custom_cycler2 = cycler(
-    color=["tab:grey", "tab:orange", ColorCode.green, ColorCode.turquoise, "m"]
-)
-#
-myFRLcolors = [
-    "tab:grey",
-    "tab:orange",
-    ColorCode.cyan,
-    ColorCode.green,
-    "m",
-    "y",
-    "k",
-    "r",
-    "b",
-    "tab:purple",
-]
-
-custom_cycler = cycler(color=myFRLcolors)
 
 
-def plot_concentration_and_spectra(result_datasets: list, cycler=None):
+def plot_concentration_and_spectra(result_datasets: list, cycler=None, das_cycler=None):
     fig, axes = plt.subplots(1, 3, figsize=(15, 4))
     for result_dataset in result_datasets:
         plot_concentrations(
             result_dataset, axes[0], center_λ=0, linlog=True, cycler=cycler
         )
         plot_sas(result_dataset, axes[1], cycler=cycler)
-        plot_das(result_dataset, axes[2], cycler=cycler)
+        plot_das(result_dataset, axes[2], cycler=das_cycler)
+    axes[0].set_xlabel("Time (ps)")
+    axes[0].set_ylabel("")
+    axes[0].axhline(0, color="k", linewidth=1)
+    axes[1].set_xlabel("Wavelength (nm)")
+    axes[1].set_ylabel("SADS (mOD)")
+    axes[1].set_title("SADS")
+    axes[2].set_xlabel("Wavelength (nm)")
+    axes[2].set_ylabel("DADS (mOD)")
+    axes[2].set_title("DADS")
+    axes[1].axhline(0, color="k", linewidth=1)
+    axes[0].annotate("A", xy=(-0.05, 1.02), xycoords="axes fraction", fontsize=16)
+    axes[1].annotate("B", xy=(-0.05, 1.02), xycoords="axes fraction", fontsize=16)
+    axes[2].annotate("C", xy=(-0.05, 1.02), xycoords="axes fraction", fontsize=16)
+
     return fig, axes
 
 
